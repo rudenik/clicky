edff38dec8ac0a24b3ac77b9134f9701eca8cfb1
Simon Cain <rudenik@gmail.com>
2018-09-02 15:20:25 -0400

    changed one line on the header section

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ import "./header.css";
 const header = props => (
     <header className="header">
         <h1>Clicky Clack image attack!</h1>
-    <h2>Click on an image to earn points, but don't click on any more than once!
+    <h2>Click on an image to earn points, but don't click on it twice!
     </h2>
     </header>
 
